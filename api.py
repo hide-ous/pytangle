e01411dc19531ac08f2416bd6d60d7ef97df8960
hide-ous <mattia.samory@gmail.com>
2020-05-18 17:37:04 +0200

    * enhanced connectivity with pagination + api stub

---
+++ api.py
@@ -0,0 +1,28 @@
+from connectivity import make_request, make_request_1_every_10s, iterate_request, make_request_1_every_30s
+
+
+def lists(token):
+    param_dict = {'token': token}
+    response = make_request('https://api.crowdtangle.com/lists', param_dict)
+    return response['result']['lists']
+
+
+def posts(*args):
+    return iterate_request(args, 'https://api.crowdtangle.com/posts', 'post', make_request_1_every_10s)
+
+
+def links(*args):
+    return iterate_request(args, 'https://api.crowdtangle.com/links', 'links', make_request_1_every_30s)
+
+
+def leaderboard(*args):
+    return iterate_request(args, 'https://api.crowdtangle.com/leaderboard', 'leaderboard', make_request_1_every_10s)
+
+
+def search(*args):
+    return iterate_request(args, 'https://api.crowdtangle.com/search', 'leaderboard', make_request_1_every_10s)
+
+
+def list_id_or_account(*args):
+    list_id = args.pop('listId')
+    return iterate_request(args, 'https://api.crowdtangle.com/lists/:{}/accounts'.format(list_id), 'accounts', make_request_1_every_10s)
--- connectivity.py
+++ connectivity.py
@@ -1,13 +1,11 @@
 import time
 import json
 import requests
-import itertools
-from datetime import datetime, timedelta
 from ratelimit import limits, sleep_and_retry
 
 import logging
-import sys
 
+# TODO: add config for logger name
 logger = logging.getLogger('CT')
 
 ONE_SECOND = 1
@@ -18,26 +16,43 @@ THIRTY_SECONDS = 30
 
 @sleep_and_retry
 @limits(calls=1, period=TEN_SECONDS)
-def make_request_1_every_10s(uri, params, max_retries=5, logger=logger):
-    return make_request(uri, params, max_retries=5, logger=logger)
+def make_request_1_every_10s(uri, params, max_retries=5):
+    return make_request(uri, params, max_retries=5)
 
 @sleep_and_retry
 @limits(calls=1, period=THIRTY_SECONDS)
-def make_request_1_every_30s(uri, params, max_retries=5, logger=logger):
-    return make_request(uri, params, max_retries=5, logger=logger)
-
-def make_request(uri, params, max_retries=5, logger=logger):
+def make_request_1_every_30s(uri, params, max_retries=5):
+    return make_request(uri, params, max_retries=5)
+#TODO: add config for max_retries
+def make_request(uri, params, max_retries=5):
     current_tries = 0
     last_exception = None
     while current_tries < max_retries:
         try:
             response = requests.get(uri, params=params)
             logger.debug(response)
+            #TODO: handle errors meaningfully
             assert response.status_code == 200
             return json.loads(response.content.decode('utf-8'))
         except Exception as e:
             logger.debug(e)
             last_exception = e
+            #TODO: add config for how long to wait
             time.sleep(1)
             current_tries += 1
     raise last_exception
+
+
+def iterate_request(param_dict, endpoint, response_field, request_fun):
+    next_page = endpoint
+    while next_page:
+        response = request_fun(next_page, param_dict)
+        next_page = None
+        for thing in response['result'][response_field]):
+            yield thing
+        if 'pagination' in response['result']:
+            if 'nextPage' in response['result']['pagination']:
+                next_page = response['result']['pagination']['nextPage']
+                param_dict={}
+
+
