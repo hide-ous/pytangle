a75b931631ecf9ea9f079f05dc3b8db57e84897c
hide-ous <mattia.samory@gmail.com>
2020-07-08 13:57:08 +0200

    * boilerplate code to handle crowdtangle-specific error codes

--- connectivity.py
+++ connectivity.py
@@ -1,5 +1,7 @@
 import time
 import json
+from collections import defaultdict
+
 import requests
 from ratelimit import limits, sleep_and_retry, RateLimitException
 
@@ -37,11 +39,39 @@ def make_request(uri, params, max_retries=5):
             return json.loads(response.content.decode('utf-8'))
         except requests.exceptions.HTTPError as errh:
             logger.error("Http Error:", errh)
-            if errh.response.status_code == 429:  # rate limit exceeded
+            error_details = defaultdict(None, json.loads(errh.response.content))
+            error_details['http_status'] = errh.response.status_code
+            error_message = error_details['message']
+            error_ct_status = error_details['ct_status']
+            error_http_status = error_details['http_status']
+            error_code = error_details['code']
+            error_url = error_details['url']
+
+            logger.debug("error status (HTTP):", error_http_status,
+                         "error status (CrowdTangle):", error_ct_status,
+                         "error code:", error_code,
+                         "error message:", error_message,
+                         "error url:", error_url)
+
+            if error_http_status == 429:  # rate limit exceeded
                 raise RateLimitException()  # should be handled by ratelimit
-            elif errh.response.status_code / 100 == 4:  # 4XX error other than 429
+            elif error_code == 20:  # Unknown Parameter
                 raise errh
-            elif errh.response.status_code / 100 == 5:  # 5XX error
+            elif error_code == 21:  # Illegal Parameter Value
+                raise errh
+            elif error_code == 22:  # Missing Parameter
+                raise errh
+            elif error_code == 30:  # Missing Token
+                raise errh
+            elif error_code == 31:  # Invalid Token
+                raise errh
+            elif error_code == 40:  # Does Not Exist
+                raise errh
+            elif error_code == 41:  # Not Allowed
+                raise errh
+            elif error_http_status / 100 == 4:  # 4XX error other than 429
+                raise errh
+            elif error_http_status / 100 == 5:  # 5XX error
                 # TODO: add config for how long to wait
                 time.sleep(60)
             else:
@@ -66,7 +96,7 @@ def make_request(uri, params, max_retries=5):
 
 
 def iterate_request(param_dict, endpoint, response_field, request_fun):
-    #FIXME: poor design, hiding parameters in data
+    # FIXME: poor design, hiding parameters in data
     count = -1
     if "batchSize" in param_dict:
         if "count" in param_dict:
@@ -84,8 +114,8 @@ def iterate_request(param_dict, endpoint, response_field, request_fun):
             if (count == -1) or (n_yielded < count):
                 yield result
                 n_yielded += 1
-        if ('pagination' in response['result']) and\
-                ("nextPage" in response["result"]['pagination']) and\
+        if ('pagination' in response['result']) and \
+                ("nextPage" in response["result"]['pagination']) and \
                 ((count == -1) or (n_yielded < count)):
             next_page = response['result']['pagination']['nextPage']
             param_dict = dict()
