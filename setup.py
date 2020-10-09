b9ae44eeee63adae30057c4d0eb5399e41e2dbf7
hide-ous <mattia.samory@gmail.com>
2020-10-09 19:26:43 +0200

    * updated release link

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hide-ous/pytangle/",
-    download_url="https://github.com/hide-ous/pytangle/archive/v0.0.1.tar.gz",
+    download_url="https://github.com/hide-ous/pytangle/archive/v0.0.2.tar.gz",
     keywords=['crowdtangle', 'api', 'wrapper'],
     packages=setuptools.find_packages(),
     install_requires=[
